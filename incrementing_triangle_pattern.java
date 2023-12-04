35d2bfac37380ce2ca037982bcf7e2d190f7288b
SubhamPaul21 <subhampaul12@gmail.com>
2023-12-04 19:27:47 +0530

    adds incrementing triangle pattern

--- incrementing_triangle_pattern.java
+++ incrementing_triangle_pattern.java
@@ -1,3 +1,11 @@
 public class incrementing_triangle_pattern {
     
+    public static void main(String[] args) {
+        for(int i=0; i<=3; i++) {
+            for(int j=0; j<=i; j++) {
+                System.out.print("0");
+            }
+            System.out.println();
+        }    
+    }
 }
