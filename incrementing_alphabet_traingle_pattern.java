1c2b2c7898540a4f67cec234270b7c35319f7051
SubhamPaul21 <subhampaul12@gmail.com>
2023-12-04 20:02:54 +0530

    modifies increment alphabet traingle pattern code

--- incrementing_alphabet_traingle_pattern.java
+++ incrementing_alphabet_traingle_pattern.java
@@ -1,17 +1,28 @@
+// Constraints
+// 0 <= N <= 26
+
+
 public class incrementing_alphabet_traingle_pattern {
     
     public static void incrementing_alphabet_traingle(int n) {
         //char character = (char) n;
         //int number = (int) character;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<i+1; j++) {
-                System.out.print((char)(65 + i));
+
+        if( 0 <= n && n <= 26) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<i+1; j++) {
+                    System.out.print((char)(65 + i));
+                }
+                System.out.println();
             }
-            System.out.println();
+        } else {
+            System.out.println("Out of bound!");
         }
+        
     }
 
     public static void main(String[] args) {
         incrementing_alphabet_traingle(7);
+        incrementing_alphabet_traingle(50);
     }
 }
