4e874860517e914bdf3c7ca3240b801c2c22e387
SubhamPaul21 <subhampaul12@gmail.com>
2023-12-04 20:20:25 +0530

    modifies alphabet increment pattern code

--- alphabet_increment_pattern.java
+++ alphabet_increment_pattern.java
@@ -1,12 +1,19 @@
+// 0 <= N <= 13
+
 public class alphabet_increment_pattern {
     
     public static void pattern_function(int n) {
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<=2*i; j++) {
-                System.out.print((char) (65 + j) + " ");
+
+        if (0 <= n && n <= 13) {
+            for(int i=0; i<n; i++) {
+                for(int j=i; j<=2*i; j++) {
+                    System.out.print((char) (65 + j) + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
+        } else {
+            System.out.println("Out of bound!");
+        } 
     }
 
     public static void main(String[] args) {
