ad6ce18ad4078fe45e27f30447053d46155d43b9
SubhamPaul21 <subhampaul12@gmail.com>
2023-12-04 19:43:29 +0530

    adds incrementing number triangle pattern code

---
+++ incrementing_different_number_triangle_pattern.java
@@ -0,0 +1,11 @@
+public class incrementing_different_number_triangle_pattern {
+    
+    public static void main(String[] args) {
+        for(int i=0; i<=4; i++) {
+            for(int j=0; j<i+1;j++) {
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }    
+    }
+}
