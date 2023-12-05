f0880651cc35f20ffda486d608bdb076b24ba814
SubhamPaul21 <subhampaul12@gmail.com>
2023-12-05 11:22:42 +0530

    modifies kite star pattern code

--- kite_star_pattern.java
+++ kite_star_pattern.java
@@ -1,41 +1,46 @@
 public class kite_star_pattern {
     
     public static void pattern_function(int n) {
-        if (1<=n && n<=49) {
-            int mid = n/2;
-            // Part 1
-            //row loop
-            for(int i=0; i<=mid; i++) {
-                // dot loop
-                for(int j=0; j<mid-i; j++) {
-                    System.out.print(". ");
+        if (n % 2 != 0) {
+            if (1<=n && n<=49) {
+                int mid = n/2;
+                // Part 1
+                //row loop
+                for(int i=0; i<=mid; i++) {
+                    // dot loop
+                    for(int j=0; j<mid-i; j++) {
+                        System.out.print(". ");
+                    }
+                    // star loop
+                    for(int k=0; k<2*i+1; k++) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
                 }
-                // star loop
-                for(int k=0; k<2*i+1; k++) {
-                    System.out.print("* ");
-                }
-                System.out.println();
-            }
 
-            // Part 2
-            // row loop
-            for(int i=1; i<=mid; i++) {
-                // dot loop
-                for(int j=0; j<i; j++) {
-                    System.out.print(". ");
+                // Part 2
+                // row loop
+                for(int i=1; i<=mid; i++) {
+                    // dot loop
+                    for(int j=0; j<i; j++) {
+                        System.out.print(". ");
+                    }
+                    // star loop
+                    for(int k=n-2*i; k>0; k--) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
                 }
-                // star loop
-                for(int k=n-2*i; k>0; k--) {
-                    System.out.print("* ");
-                }
-                System.out.println();
+            } else {
+                System.out.println("Out of bound!");
             }
         } else {
-            System.out.println("Out of bound!");
+            System.out.println("N is not an odd number!");
         }
+        
     }
 
     public static void main(String[] args) {
-        pattern_function(5);
+        pattern_function(7);
     }
 }
